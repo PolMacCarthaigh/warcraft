dae23eb4f2515ee964e17aad5440f627ef044cf6
PolMacCarthaigh <paulmccarthy2010.ca>
2016-07-28 00:35:59 +0000

    another fix

--- spec/test_11.rb
+++ spec/test_11.rb
@@ -20,8 +20,18 @@ require 'spec_helper'
   #Damage buildings
   describe "footman" do
     it "Footman damage buildings by 1/2" do
-      @barracks.damage(5)
-      expect(@barracks.health_points).to eq(495) 
+      @footman = Footman.new
+      expect(@barracks).to receive(:damage).with(10)
+      @footman.attack!(@barracks)
+      # expect(@barracks.health_points).to eq(495) 
+    end
+
+    it "damages regularly when its not a building" do
+      @footman = Footman.new
+      @footman2 = Footman.new
+      expect(@footman2).to receive(:damage).with(10)
+      @footman.attack!(@footman2)
+      # expect(@footman2.health_points).to eq(60) 
     end
   end
   
